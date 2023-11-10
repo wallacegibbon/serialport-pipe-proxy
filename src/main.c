e37f92e9493a7d8b4aceff73a4ca8c1fe43a0633
wallace <wallacegibbon@aliyun.com>
2023-11-10 10:15:37 +0800

    Refactor: Code got cleaned up by introducing new struct to store global application states.

--- src/main.c
+++ src/main.c
@@ -10,69 +10,93 @@
 #define SERIALPORT_READ_BUFFER_SIZE 4096
 #define STDIN_READ_BUFFER_SIZE 4096
 
-/// flag protected by a mutex for checking whether this program has finished.
-struct {
-	pthread_mutex_t mutex;
-	int flag;
-} port_active;
-
-/// the global variable shared by 2 threads.
-struct sp_port *port;
-
-struct application_params {
-	const char *serialport_device;
-	int baudrate;
-	const char *start_string;
-	const char *end_string;
+// clang-format off
+struct application {
+	/// parameters
+	const char				*serialport_device;
+	int					baudrate;
+	const char				*start_string;
+	const char				*end_string;
+	/// running states
+	struct sp_port				*serialport;
+	int					running_flag;
+	pthread_mutex_t				running_flag_lock;
 };
+// clang-format on
 
-void application_params_print(struct application_params *params) {
-	fprintf(stderr, "application params\n\tport:%s, baudrate:%d, startstring:%s, endstring:%s\n",
-		params->serialport_device, params->baudrate, params->start_string, params->end_string);
+struct application app;
+
+void app_initialize(struct application *self) {
+	int r;
+
+	pthread_mutex_init(&self->running_flag_lock, NULL);
+	self->running_flag = 1;
+
+	/// Open the serial port before starting serial port reading thread to avoid some error.
+	r = serialport_open(&self->serialport, self->serialport_device, self->baudrate);
+	if (r == -1)
+		exit_info(r, "failed getting port by name \"%s\"\n", self->serialport_device);
+	if (r == -2)
+		exit_info(r, "failed opening port \"%s\"\n", self->serialport_device);
 }
 
-void parse_arguments(int argc, const char **argv, struct application_params *params) {
+void app_cleanup(struct application *self) {
+	int r;
+	pthread_mutex_destroy(&self->running_flag_lock);
+	r = sp_close(self->serialport);
+	if (r < 0)
+		exit_info(r, "failed closing port %s\n", self->serialport_device);
+}
+
+void app_describe(struct application *self) {
+	fprintf(stderr, "application params\n\tport:%s, baudrate:%d, startstring:%s, endstring:%s\n",
+		self->serialport_device, self->baudrate, self->start_string, self->end_string);
+}
+
+void app_running_flag_set(struct application *self, int new_value) {
+	pthread_mutex_lock(&self->running_flag_lock);
+	self->running_flag = new_value;
+	pthread_mutex_unlock(&self->running_flag_lock);
+}
+
+int app_running_flag_get(struct application *self) {
+	int is_active;
+	pthread_mutex_lock(&self->running_flag_lock);
+	is_active = self->running_flag;
+	pthread_mutex_unlock(&self->running_flag_lock);
+	return is_active;
+}
+
+void parse_arguments(int argc, const char **argv, struct application *app) {
 	if (argc < 3)
 		exit_info(1, "Usage sample: sp-pipe /dev/ttyUSB0 115200 ['start string'] ['end string']\n");
 
-	params->serialport_device = argv[1];
-	params->baudrate = atoi(argv[2]);
-	params->start_string = NULL;
-	params->end_string = NULL;
+	app->serialport_device = argv[1];
+	app->baudrate = atoi(argv[2]);
+	app->start_string = NULL;
+	app->end_string = NULL;
 
 	if (argc >= 4)
-		params->start_string = argv[3];
+		app->start_string = argv[3];
 	if (argc >= 5)
-		params->end_string = argv[4];
-}
-
-void port_active_set(int new_value) {
-	pthread_mutex_lock(&port_active.mutex);
-	port_active.flag = new_value;
-	pthread_mutex_unlock(&port_active.mutex);
-}
-
-int port_active_get() {
-	int is_active;
-	pthread_mutex_lock(&port_active.mutex);
-	is_active = port_active.flag;
-	pthread_mutex_unlock(&port_active.mutex);
-	return is_active;
+		app->end_string = argv[4];
 }
 
 // clang-format off
 enum serialport_fsm_state {FSM_WAITING_FOR_START, FSM_NORMAL1, FSM_NORMAL2, FSM_END};
 // clang-format on
 
+// clang-format off
 struct serialport_fsm {
-	unsigned char buffer[SERIALPORT_READ_BUFFER_SIZE + 1];
-	int cursor;
-	int buffer_end;
-	enum serialport_fsm_state state;
-	struct str_detector detector;
-	const char *start_string;
-	const char *end_string;
+	unsigned char			buffer[SERIALPORT_READ_BUFFER_SIZE + 1];
+	int				cursor;
+	int				buffer_end;
+	enum serialport_fsm_state	state;
+	struct str_detector		detector;
+	const char			*start_string;
+	const char			*end_string;
 };
+// clang-format on
 
 void s_fsm_initialize(struct serialport_fsm *self, const char *start_string, const char *end_string) {
 	if (start_string != NULL) {
@@ -139,7 +163,7 @@ int s_fsm_normal2(struct serialport_fsm *self) {
 void s_fsm_fill_more(struct serialport_fsm *self) {
 	if (self->buffer_end == 0 || self->cursor == self->buffer_end) {
 		self->cursor = 0;
-		self->buffer_end = sp_blocking_read(port, self->buffer, SERIALPORT_READ_BUFFER_SIZE, 100);
+		self->buffer_end = sp_blocking_read(app.serialport, self->buffer, SERIALPORT_READ_BUFFER_SIZE, 100);
 		if (self->buffer_end < 0)
 			exit_info(11, "failed reading from serial port\n");
 
@@ -164,73 +188,60 @@ int s_fsm_step(struct serialport_fsm *self) {
 	}
 }
 
-void *serialport_data_handler(struct application_params *app_params) {
+void *serialport_data_handler(void *data) {
 	struct serialport_fsm s_fsm;
 
-	s_fsm_initialize(&s_fsm, app_params->start_string, app_params->end_string);
+	s_fsm_initialize(&s_fsm, app.start_string, app.end_string);
 
-	while (port_active_get() || (s_fsm.state != FSM_END && s_fsm.cursor != s_fsm.buffer_end)) {
+	while (app_running_flag_get(&app) || (s_fsm.state != FSM_END && s_fsm.cursor != s_fsm.buffer_end)) {
 		if (s_fsm_step(&s_fsm))
 			break;
 	}
 
-	port_active_set(0);
+	app_running_flag_set(&app, 0);
 }
 
 void *stdin_data_handler(void *data) {
 	unsigned char buffer[STDIN_READ_BUFFER_SIZE + 1];
 	int r;
 
-	while (port_active_get()) {
+	while (app_running_flag_get(&app)) {
 		r = fread(buffer, 1, STDIN_READ_BUFFER_SIZE, stdin);
 		if (r < STDIN_READ_BUFFER_SIZE) {
-			port_active_set(0);
+			app_running_flag_set(&app, 0);
 			if (!feof(stdin) && ferror(stdin))
 				exit_info(3, "writting to serialport error: %d\n", errno);
 		}
-		r = sp_blocking_write(port, buffer, r, 0);
+		r = sp_blocking_write(app.serialport, buffer, r, 0);
 		if (r < 0)
 			exit_info(2, "failed writting to serialport\n");
 	}
 
 	/*
-	while (port_active_get()) {
+	while (app_running_flag_get(&app)) {
 		if (!feof(stdin)) {
 			r = fgetc(stdin);
-			r = sp_blocking_write(port, &r, 1, 0);
+			r = sp_blocking_write(app.serialport, &r, 1, 0);
 			if (r < 0)
 				exit_info(2, "failed writting to serialport\n");
 		} else {
-			port_active_set(0);
+			app_running_flag_set(&app, 0);
 		}
 	}
 	*/
 }
 
-void initialize_port(struct application_params *app_params) {
-	int r;
-	r = serialport_open(&port, app_params->serialport_device, app_params->baudrate);
-	if (r == -1)
-		exit_info(r, "failed getting port by name \"%s\"\n", app_params->serialport_device);
-	if (r == -2)
-		exit_info(r, "failed opening port \"%s\"\n", app_params->serialport_device);
-}
+typedef void *(*pthread_start_routine)(void *);
 
 int main(int argc, const char **argv) {
 	pthread_t stdin_thread, serialport_thread;
 	int r;
-	struct application_params params;
 
-	parse_arguments(argc, argv, &params);
-	// application_params_print(&params);
+	parse_arguments(argc, argv, &app);
+	app_initialize(&app);
+	// app_describe(&app);
 
-	/// Open the serial port before starting serial port reading thread to avoid some error.
-	initialize_port(&params);
-
-	pthread_mutex_init(&port_active.mutex, NULL);
-	port_active.flag = 1;
-
-	r = pthread_create(&serialport_thread, NULL, (void *(*)(void *))serialport_data_handler, &params);
+	r = pthread_create(&serialport_thread, NULL, (pthread_start_routine)serialport_data_handler, NULL);
 	if (r)
 		exit_info(2, "failed creating serialport thread\n");
 
@@ -241,11 +252,7 @@ int main(int argc, const char **argv) {
 	pthread_join(stdin_thread, NULL);
 	pthread_join(serialport_thread, NULL);
 
-	pthread_mutex_destroy(&port_active.mutex);
-
-	r = sp_close(port);
-	if (r < 0)
-		exit_info(r, "failed closing port %s\n", params.serialport_device);
+	app_cleanup(&app);
 
 	return 0;
 }
